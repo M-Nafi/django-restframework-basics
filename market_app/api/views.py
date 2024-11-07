d3c6cbe111dc7725a54fda97cc7a66bbf24d34fa
M-Nafi <nafi-m@gmx.de>
2024-11-07 18:33:13 +0100

    serializer: single-view, create/put methods

--- market_app/api/views.py
+++ market_app/api/views.py
@@ -7,15 +7,10 @@ from market_app.models import Market
 @api_view(['GET', 'POST'])
 def markets_view(request):
 
-
-
-
     if request.method == 'GET':
         markets = Market.objects.all()
         serializer = MarketSerializer(markets, many=True)
-        return Response(serializer.data)   
-       
-    
+        return Response(serializer.data)    
     
     if request.method == 'POST':
        serializer = MarketSerializer(data=request.data)
@@ -24,3 +19,30 @@ def markets_view(request):
            return Response(serializer.data)
        else:
            return Response(serializer.errors)
+       
+
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def market_single_view(request, pk):
+
+    if request.method == 'GET':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market)
+        return Response(serializer.data)   
+    
+    
+    if request.method == 'PUT':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market, data=request.data, partial=True)
+        if serializer.is_valid():
+           serializer.save()
+           return Response(serializer.data)
+        else:
+           return Response(serializer.errors)
+        
+
+    if request.method == 'DELETE':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market)
+        market.delete()
+        return Response(serializer.data)   
