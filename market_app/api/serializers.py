7b6905a784d9294bdcbb30312f687444b44abaca
M-Nafi <nafi-m@gmx.de>
2024-11-08 12:30:46 +0100

    serializer: test with postman and added nested serializer. working on adding seller view

--- market_app/api/serializers.py
+++ market_app/api/serializers.py
@@ -1,10 +1,24 @@
 from rest_framework import serializers
 from market_app.models import Market
 
+ 
+def validate_no_x(value):
+    errors = []
+
+    if 'X' in value:
+        errors.append('no X in location')
+    if 'Y' in value:
+        errors.append('no Y in location')
+    
+    if errors:
+        raise serializers.ValidationError(errors)      
+    return value
+
+
 class MarketSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
-    location = serializers.CharField(max_length=255)
+    location = serializers.CharField(max_length=255, validators=[validate_no_x])
     description = serializers.CharField()
     net_worth = serializers.DecimalField(max_digits=100, decimal_places=2)
 
@@ -18,3 +32,14 @@ class MarketSerializer(serializers.Serializer):
         instance.net_worth = validated_data.get('net_worth', instance.net_worth)
         instance.save()
         return instance
+    
+      # diese funktion ist wichtig und kann bei join verwendet werden, wenn 
+    # man nicht möchte, dass etwas bestimmtes eingegeben werden soll. 
+    #  
+    # def validate_location(self, value):
+    #     if 'X' in value:
+    #         raise serializers.ValidationError('das X gehört da nicht rein!')
+    #     return value
+
+class SellerDetailSerializer(serializers.Serializer):
+    pass
