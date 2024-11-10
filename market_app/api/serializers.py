0eb4934eebb16ea17746a37515d6293e9caca64d
Nafi Müftüoglu <140119132+M-Nafi@users.noreply.github.com>
2024-11-10 18:22:30 +0100

    added model-serializer in serializers

--- market_app/api/serializers.py
+++ market_app/api/serializers.py
@@ -1,35 +1,23 @@
 from rest_framework import serializers
 from market_app.models import Market, Seller, Product
 
- 
-def validate_no_x(value):
-    errors = []
-    if 'X' in value:
-        errors.append('no X in location')
-    if 'Y' in value:
-        errors.append('no Y in location')    
-    if errors:
-        raise serializers.ValidationError(errors)      
-    return value
 
+class MarketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Market
+        fields = '__all__'
 
-class MarketSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=255)
-    location = serializers.CharField(max_length=255, validators=[validate_no_x])
-    description = serializers.CharField()
-    net_worth = serializers.DecimalField(max_digits=100, decimal_places=2)
-
-    def create(self, validated_data):
-        return Market.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.location = validated_data.get('location', instance.location)
-        instance.description = validated_data.get('description', instance.description)
-        instance.net_worth = validated_data.get('net_worth', instance.net_worth)
-        instance.save()
-        return instance
+    def validate_name(self, value):
+        errors = []
+        
+        if 'X' in value:
+            errors.append('no X in location')
+        if 'Y' in value:
+            errors.append('no Y in location')    
+        if errors:
+            raise serializers.ValidationError(errors)      
+        
+        return value
     
       # diese funktion ist wichtig und kann bei join verwendet werden, wenn 
     # man nicht möchte, dass etwas bestimmtes eingegeben werden soll. 
@@ -46,56 +34,12 @@ class SellerDetailSerializer(serializers.Serializer):
     markets = MarketSerializer(many=True, read_only=True)
 
 
-class SellerCreateSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=255)
-    contact_info = serializers.CharField()
-    markets = serializers.ListField(child=serializers.IntegerField(), write_only=True)
+class SellerCreateSerializer(serializers.ModelSerializer):
+     class Meta:
+        model = Seller
+        fields = '__all__'
 
-    def validate_markets(self, value):
-        markets = Market.objects.filter(id__in=value)
-        if len(markets) !=len(value):
-            raise serializers.ValidationError("some or more Marketids not found!")
-        return value
-    
-    def create(self, validated_data):
-        market_ids=validated_data.pop('markets')
-        seller = Seller.objects.create(**validated_data)
-        markets = Market.objects.filter(id__in=market_ids)
-        seller.markets.set(markets)
-        return seller
-    
-
-class ProductDetailSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=255)
-    description = serializers.CharField()
-    market = serializers.IntegerField(write_only=True)
-    seller = serializers.IntegerField(write_only=True)  
-    price = serializers.DecimalField(max_digits=50, decimal_places=2)
-
-    def validate_market(self, value):
-        if not Market.objects.filter(id=value).exists():
-            raise serializers.ValidationError("Invalid market id")
-        return value
-
-    def validate_seller(self, value):
-        if not Seller.objects.filter(id=value).exists():
-            raise serializers.ValidationError("Invalid seller id")
-        return value
-
-    def create(self, validated_data):
-        market_id = validated_data.pop('market')
-        seller_id = validated_data.pop('seller')
-        product = Product.objects.create(market_id=market_id, seller_id=seller_id, **validated_data)
-        return product
-    
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.market = validated_data.get('market', instance.market)
-        instance.seller = validated_data.get('seller', instance.seller)
-        instance.price = validated_data.get('price', instance.price)
-       
-        instance.save()
-        return instance
-    
+class ProductDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
