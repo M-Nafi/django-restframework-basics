151e6c2196ea6428bd53a178a7130ebcc94410f1
Nafi Müftüoglu <140119132+M-Nafi@users.noreply.github.com>
2024-11-11 19:46:13 +0100

    exercises with generic classes update destroy and getquery set

--- market_app/api/urls.py
+++ market_app/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MarketsView, MarketDetails, sellers_view, seller_single_view, products_view, product_single_view
+from .views import MarketsView, MarketDetails, SellerOfMarketList, sellers_view, seller_single_view, products_view, product_single_view
 
 urlpatterns = [
     path('market/', MarketsView.as_view()),
     path('market/<int:pk>/', MarketDetails.as_view(), name='market-detail'),
+    path('market/<int:pk>/sellers/', SellerOfMarketList.as_view()),
     path('seller/', sellers_view),
     path('seller/<int:pk>/', seller_single_view, name='seller-single'),
     path('product/', products_view),
--- market_app/api/views.py
+++ market_app/api/views.py
@@ -8,75 +8,22 @@ from rest_framework import mixins
 from rest_framework import generics
 
 
-class MarketsView (mixins.ListModelMixin,
-                   mixins.CreateModelMixin,                
-                   generics.GenericAPIView):
-    
+class MarketsView (generics.ListCreateAPIView):
     queryset = Market.objects.all()
     serializer_class = MarketSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-# alte get und post version ohne mixins !
-# @api_view(['GET', 'POST'])
-# def markets_view(request):
-#     if request.method == 'GET':
-#         markets = Market.objects.all()
-#         serializer = MarketHyperlinkedSerializer(markets, many=True, context={'request': request}, fields=('id', 'name', 'net_worth'))
-#         return Response(serializer.data)    
-#     if request.method == 'POST':
-#        serializer = MarketSerializer(data=request.data)
-#        if serializer.is_valid():
-#            serializer.save()
-#            return Response(serializer.data, status=status.HTTP_201_CREATED)
-#        else:
-#            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
        
-
-class MarketDetails(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
-    
+class MarketDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Market.objects.all()
     serializer_class = MarketSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+class SellerOfMarketList(generics.ListAPIView):
+    serializer_class = SellerSerializer
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        market = Market.objects.get(pk = pk)
+        return market.sellers.all()
 
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-    
-
-# alte get und post und delete version ohne mixins !
-# @api_view(['GET', 'DELETE', 'PUT'])
-# def market_single_view(request, pk):
-
-#     if request.method == 'GET':
-#         market = Market.objects.get(pk=pk)
-#         serializer = MarketSerializer(market, context={'request': request})
-#         return Response(serializer.data)  
-#     if request.method == 'PUT':
-#         market = Market.objects.get(pk=pk)
-#         serializer = MarketSerializer(market, data=request.data, partial=True)
-#         if serializer.is_valid():
-#            serializer.save()
-#            return Response(serializer.data)
-#         else:
-#            return Response(serializer.errors)
-#     if request.method == 'DELETE':
-    #     market = Market.objects.get(pk=pk)
-    #     serializer = MarketSerializer(market)
-    #     market.delete()
-    #     return Response(serializer.data)   
-    
 
 @api_view(['GET', 'POST'])
 def sellers_view(request):
